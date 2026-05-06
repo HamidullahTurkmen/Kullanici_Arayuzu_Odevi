4514ed6fe1cbe66aae211bf09d95427c3f518686
Hamidullah Türkmen <35694025+HamidullahTurkmen@users.noreply.github.com>
2026-05-06 09:32:32 +0300

    Add files via upload

---
+++ Google_sheet.js
@@ -0,0 +1,41 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbz0ZK0U1CNmmHn8wZEbheqqNSZHgjAZ6HcvvgZ8uX83N1UEDNdmPM7iP2Ymu-le69Dg/exec";
+
+const form = document.forms["contact-form"];
+const mesaj = document.getElementById("mesaj");
+const submitButton = document.getElementById("submit");
+const loadingScreen = document.getElementById("loadingScreen");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  submitButton.disabled = true;
+  submitButton.innerText = "Kaydediliyor...";
+  mesaj.style.color = "#2563eb";
+  mesaj.innerText = "Bilgileriniz kaydediliyor...";
+
+  loadingScreen.style.display = "flex";
+
+  fetch(scriptURL, {
+    method: "POST",
+    mode: "no-cors",
+    body: formData
+  })
+  .then(function() {
+    setTimeout(function() {
+      window.location.href = "success.html";
+    }, 1200);
+  })
+  .catch(function(error) {
+    loadingScreen.style.display = "none";
+
+    mesaj.style.color = "#dc2626";
+    mesaj.innerText = "Kayıt sırasında hata oluştu. Lütfen tekrar deneyiniz.";
+
+    submitButton.disabled = false;
+    submitButton.innerText = "Kayıt Et";
+
+    console.error("Error!", error.message);
+  });
+});
